8f8a6ba56597924d987c3e19890f69a17bc80915
Stacy Potten <stacylynnaz@gmail.com>
2020-11-10 09:51:01 -0700

    Add fetch to post new urls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,12 @@
 export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+    .then(response => response.json())
 }
+
+export const addUrls = (urlInfo) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(urlInfo)
+  });
+  .then(response => response.json())
